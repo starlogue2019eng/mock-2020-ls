6e3df453c262622d4cc786eef7f1bdd985468a62
starlogue2019eng <starlogue2019eng@gmail.com>
2020-03-13 18:47:07 +0800

    adapted to LS

--- assets/main.js
+++ assets/main.js
@@ -4,14 +4,14 @@ var app = new Vue({
     message: "",
     candidate: {
       "cand_num_3": null,
-      "Phone_num": null,
+      "phone_num": null,
     }
   },
   mounted: function () { 
-    // var cand_num_3 = prompt("考生編號尾三位數字"); //ask for login detail
+    var cand_num_3 = prompt("考生編號尾三位數字"); //ask for login detail
     // var phone_num = prompt("電話號碼");
-    var cand_num_3 = "999";
-    var phone_num = 92345678;
+    // var cand_num_3 = "031";
+    // var phone_num = 92345678;
 
     var self = this;
     
@@ -22,14 +22,15 @@ var app = new Vue({
     .done(function(data) {
       data = Papa.parse(data, {header: true}).data;
       data.forEach(element => {
-        if (element.cand_num_3 == cand_num_3 && element.Phone_num == phone_num) {
+        // if (element.cand_num_3 == cand_num_3 && element.phone_num == phone_num) {
+        if (element.cand_num_3 == cand_num_3) {
           console.log(element);
           self.candidate = element;
         }
       });
       // if data does not match login detail, then go to 404
-      if (data.some(candidate => candidate.cand_num_3 == cand_num_3 && candidate.Phone_num ==
-        phone_num)) {
+      // if (data.some(candidate => candidate.cand_num_3 == cand_num_3 && candidate.phone_num == phone_num)) {
+      if (data.some(candidate => candidate.cand_num_3 == cand_num_3)) {
         document.querySelector("body").style.removeProperty("visibility");
         return true;
       } else {
